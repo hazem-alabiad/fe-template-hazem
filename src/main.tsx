cca3780418a63a52b9bdaee492510bc9aebc758f
Hazem Alabiad <hazem.alabiad@getir.com>
2023-03-30 03:26:04 +0300

    chore: setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import { App } from "./components/App";
 import { LocaleProvider } from "./contexts/LocaleContext";
@@ -7,7 +9,10 @@ import { LocaleProvider } from "./contexts/LocaleContext";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LocaleProvider>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
     </LocaleProvider>
   </React.StrictMode>,
 );
