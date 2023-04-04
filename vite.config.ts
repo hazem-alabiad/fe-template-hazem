29bbe95ecc0abe24b0546daa11fd32f1af0fe5da
Hazem Alabiad <hazem.alabiad@getir.com>
2023-04-04 23:43:39 +0300

    chore: add dev tools and update tsconfig settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { reactClickToComponent } from "vite-plugin-react-click-to-component";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactClickToComponent(), svgr(), ViteImageOptimizer()],
 });
