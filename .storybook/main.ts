b82529df2134c828fd55f5c1c9873b39f1b1a874
Hazem Alabiad <hazem.alabiad@getir.com>
2023-04-15 04:42:54 +0300

    chore: upgrade node & storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+  ],
+  "docs": {
+    "autodocs": true,
+  },
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {},
+  },
+  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+};
+export default config;
